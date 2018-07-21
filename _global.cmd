f80e614e2fc9f26e9a49ccd4a1eb1a7df2db4fd6
Peter <peter@minskio.co.uk>
2018-07-21 16:53:26 +0100

    Use dedicated directory
    To keep things clean, of course

--- _global.cmd
+++ _global.cmd
@@ -9,7 +9,7 @@ rem intro
 echo off
 title Game Save Symlinker
 cls
-if "%gamename%" == "" goto error
+if "saves\%gamename%" == "" goto error
 if "%gamesavedir%" == "" goto error
 if exist "%gamename%" goto existing
 if not exist "%gamesavedir%" goto nosavedir
@@ -27,11 +27,11 @@ pause > nul && exit
 
 :begin
 echo We're ready to start, manually make a backup and press any key to begin
-if not exist "%gamename%" mkdir "%gamename%"
+if not exist "saves\%gamename%" mkdir "saves\%gamename%"
 if not exist "%gamesavedir%" mkdir "%gamesavedir%"
-robocopy /E "%gamesavedir%" "%gamename%"
+robocopy /E "%gamesavedir%" "saves\%gamename%"
 rd /s /q "%gamesavedir%"
-cmd /C mklink /J "%gamesavedir%" "%gamename%"
+cmd /C mklink /J "%gamesavedir%" "saves\%gamename%"
 if exist *.tmp del *.tmp
 cls && echo moved savegames and set up symlink, press any key to exit.
 pause > nul && exit
