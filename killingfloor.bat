9858cea5b2a4e49cc2bcf0022ac6bbd12544fd40
Peter <peter@minskio.co.uk>
2018-07-21 17:49:56 +0100

    Add a few more gamefiles

---
+++ killingfloor.bat
@@ -0,0 +1,3 @@
+set gamename=%~n0
+set gamesavedir=%programfiles(x86)%\steam\steamapps\common\%gamename%\system
+call _global.cmd
---
+++ magicka.bat
@@ -0,0 +1,3 @@
+set gamename=%~n0
+set gamesavedir=%programfiles(x86)%\steam\steamapps\common\%gamename%\savedata
+call _global.cmd
