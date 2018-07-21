25ac53ffcaa7ac1d7f34ac190ad2f17ab292d241
Peter <peter@minskio.co.uk>
2018-07-21 18:44:28 +0100

    Change Sanctum2 to use Steam directory

--- sanctum2.bat
+++ sanctum2.bat
@@ -1,3 +1,5 @@
 set gamename=%~n0
-set gamesavedir=%userprofile%\documents\my games\%gamename%
+dir /b "%programfiles(x86)%\steam\userdata" > "%gamename%.tmp"
+set /p userid=<"%gamename%.tmp"
+set gamesavedir=%programfiles(x86)%\steam\userdata\%userid%\210770
 call _global.cmd
