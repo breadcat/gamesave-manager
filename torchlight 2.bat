f787649d5f8c770861ff5a120349b798a3d05d60
breadcat <peter@minskio.co.uk>
2019-03-09 22:30:30 +0000

    Refine Torchligh 2 save dir

--- torchlight 2.bat
+++ torchlight 2.bat
@@ -1,3 +1,5 @@
 set gamename=%~n0
-set gamesavedir=%userprofile%\documents\my games\runic games\%gamename%
+dir /b "%userprofile%\documents\my games\runic games\%gamename%\save" > "%gamename%.tmp"
+set /p userid=<"%gamename%.tmp"
+set gamesavedir=%userprofile%\documents\my games\runic games\%gamename%\save\%userid%
 call _global.cmd
