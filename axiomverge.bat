726c9f1c151074d69f5c91c6479c74285e6a8f5c
breadcat <peter@minskio.co.uk>
2020-06-21 12:43:50 +0100

    Add a couple more game definitions

---
+++ axiomverge.bat
@@ -0,0 +1,3 @@
+set gamename=%~n0
+set gamesavedir=%userprofile%\documents\savedgames\%gamename%\saves
+call _global.cmd
---
+++ falloutnv.bat
@@ -0,0 +1,3 @@
+set gamename=%~n0
+set gamesavedir=%USERPROFILE%\documents\my games\falloutnv\saves
+call _global.cmd
