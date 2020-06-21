37d3145628b04a6b8c7a32673fcae9cde1e16dc0
breadcat <peter@minskio.co.uk>
2020-06-21 20:34:16 +0100

    Last two for today

---
+++ from the depths.bat
@@ -0,0 +1,3 @@
+set gamename=%~n0
+set gamesavedir=%userprofile%\documents\%gamename%\player profiles
+call _global.cmd
---
+++ super inefficient golf.bat
@@ -0,0 +1,3 @@
+set gamename=%~n0
+set gamesavedir=%localappdata%\stickysnowball\saved\savegames\
+call _global.cmd
