12ce4e3b2119a36ef5e396a44dd11628962f2580
breadcat <peter@minskio.co.uk>
2020-06-21 13:27:49 +0100

    Allow changing of saves dir name

--- _global.cmd
+++ _global.cmd
@@ -2,14 +2,16 @@
 rem custom game symlinker script
 rem this script expects the variables 'gamename' and 'gamesavedir' to be passed to it and called.
 
-rem set current directory as destination
+rem set current directory as pre-destination
 set savedir=%cd%
+rem prefix definition
+set saveprefix=saves
 
 rem intro
 echo off
 title Game Save Symlinker
 cls
-if "saves\%gamename%" == "" goto error
+if "%saveprefix%\%gamename%" == "" goto error
 if "%gamesavedir%" == "" goto error
 if exist "%gamename%" goto existing
 if not exist "%gamesavedir%" goto nosavedir
@@ -23,17 +25,23 @@ if exist *.tmp del *.tmp
 pause > nul && goto begin
 
 :nosavedir
-echo %gamesavedir% doesn't exist, press any key to exit.
+if not exist "%saveprefix%\%gamename%" goto noactualsave
+echo %gamesavedir% doesn't exist but a synced game does.
+echo Press any key to create symlink.
+pause > nul && goto begin
+
+:noactualsave
+echo %gamesavedir% and  a synced game don't exist, press any key to exit.
 if exist *.tmp del *.tmp
 pause > nul && exit
 
 :begin
 echo We're ready to start, manually make a backup and press any key to begin
-if not exist "saves\%gamename%" mkdir "saves\%gamename%"
+if not exist "%saveprefix%\%gamename%" mkdir "%saveprefix%\%gamename%"
 if not exist "%gamesavedir%" mkdir "%gamesavedir%"
-robocopy /E "%gamesavedir%" "saves\%gamename%"
+robocopy /E "%gamesavedir%" "%saveprefix%\%gamename%"
 rd /s /q "%gamesavedir%"
-cmd /C mklink /J "%gamesavedir%" "saves\%gamename%"
+cmd /C mklink /J "%gamesavedir%" "%saveprefix%\%gamename%"
 if exist *.tmp del *.tmp
 cls && echo moved savegames and set up symlink, press any key to exit.
 pause > nul && exit
@@ -48,7 +56,7 @@ pause > nul
 if not exist backups mkdir backups
 for /f "skip=1 delims=" %%x in ('wmic os get localdatetime') do if not defined X set X=%%x
 set dy=%X:~0,4%&&set dm=%X:~4,2%&&set dd=%X:~6,2%&&set dh=%X:~8,2%&&set dn=%X:~10,2%
-"%programfiles%\7-zip\7z.exe" a -mx9 "backups\saves %dy%-%dm%-%dd%_%dh%%dn%.7z" "saves\"
+"%programfiles%\7-zip\7z.exe" a -mx9 "backups\saves %dy%-%dm%-%dd%_%dh%%dn%.7z" "%saveprefix%\"
 echo backup created, press any key to exit
 :justquit
 pause > nul && exit
